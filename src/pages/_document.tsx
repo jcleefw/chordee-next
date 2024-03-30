1438f34d14fb9172f810c81a75722750e8e80fa8
JC Lee <jc.lee@onetwo.com>
2024-03-30 21:03:54 +1100

    fix build error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin={'true'}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Neucha&family=Patrick+Hand+SC&display=swap"
